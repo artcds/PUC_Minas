649e8a714a1c421f339cd729ccd7438a83623265
artcds <38928003+artcds@users.noreply.github.com>
2020-01-30 23:41:19 -0300

    Add files via upload

--- Estatistica-Exercicios-Unidade3_1.R
+++ Estatistica-Exercicios-Unidade3_1.R
@@ -1,4 +1,6 @@
 library(BSDA)
+library(samplingbook)
+
 
 #---------------- 1 -----------------
 #Numa tentativa de melhorar o esquema de atendimento, um médico procurou estimar 
@@ -17,10 +19,12 @@ c(teste_h$conf.int[1], teste_h$conf.int[2])
 #a idade média de todos os estudantes aprovados no momento. 
 #Em uma amostra aleatória de 20 estudantes, a idade média encontrada foi de 22,9 anos. 
 #A partir de estudos passados, sabe-se que o desvio padrão é de 1,5 anos e 
-#que a população está normalmente distribuída. Qual é o intervalo de 90% de confiança da idade média da população?
+#que a população está normalmente distribuída. 
+#Qual é o intervalo de 90% de confiança da idade média da população?
 
-teste_h = tsum.test(mean.x = 22.9, s.x = 1.5, n.x = 20, conf.level = 0.90)
+teste_h = zsum.test(mean.x = 22.9, sigma.x = 1.5, n.x = 20, conf.level = 0.90)
 c(teste_h$conf.int[1], teste_h$conf.int[2])
+#22.3483 23.4517
 
 
 #---------------- 3 -----------------
@@ -28,8 +32,14 @@ c(teste_h$conf.int[1], teste_h$conf.int[2])
 #180 documentos de uma empresa cliente. Detectou que 18 documentos apresentavam falhas de algum tipo. 
 #Empregando um nível de confiança igual a 95%, encontre a estimativa intervalar do percentual 
 #de documentos da empresa que mostravam alguma falha.
-teste_h = prop.test(x = 18, n = 180, conf.level = 0.95, alternative = "two.sided")
-c(teste_h$conf.int[1], teste_h$conf.int[2])
+
+tamanho_amostra <- 180
+proporcao_amostral <- 18/tamanho_amostra
+z_95 <- 1.96
+erro_amostral <- z_95 * sqrt((proporcao_amostral * (1 - proporcao_amostral))/tamanho_amostra)
+
+c(proporcao_amostral - erro_amostral, proporcao_amostral + erro_amostral)
+#0.05617307 0.14382693
 
 
 #---------------- 4 -----------------
@@ -40,9 +50,40 @@ c(teste_h$conf.int[1], teste_h$conf.int[2])
 #Qual deve ser o tamanho da amostra  quando não há qualquer pesquisa anterior sobre o 
 #assunto que possa fornecer uma estimativa do percentual de satisfação dos assinantes?
 
+#opcao1
+sample.size.prop(e = 0.025, level = 0.9)
+#1083
+
+#opcao2
+z = (qnorm(0.95) - qnorm(0.05))/2
+p = 0.5
+e = 0.025
+ceiling(((z ** 2) * p * (1-p)) / (e ** 2))
+#1083
+
+#opcao3
+z = 1.64 #pela tabela
+ceiling(((z ** 2) * p * (1-p)) / (e ** 2))
+#1076
 
 #---------------- 5 -----------------
 #Um pesquisador deseja estimar a atual taxa média de juros cobrada por casas hipotecárias. 
 #Estudos anteriores indicam que a variância da taxa de juros é de 0,1764%. 
 #Qual deveria ser o tamanho amostral a ser estudado para obtermos uma margem de erro de 0,05% 
 #e um nível de confiança de 99%?
+
+#opcao1
+sample.size.mean(e = 0.05, S = sqrt(0.1764), level = 0.99)
+#469
+
+#opcao2
+z = (qnorm(0.995) - qnorm(0.005))/2
+s = sqrt(0.1764)
+e = 0.05
+ceiling(((z * s) / e ) ** 2)
+#469
+
+#opcao3
+z = 2.57 #pela tabela
+ceiling(((z * s) / e ) ** 2)
+#467
